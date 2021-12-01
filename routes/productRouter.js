e39df1eada1065ca7102ad2eed13a95abc383285
debernall <debernall@unal.edu.co>
2021-11-30 22:57:52 -0500

    Authentication

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -77,4 +77,153 @@ productRouter.route('/:productId')
     .catch((err) => next(err));
 })
 
+
+productRouter.route('/:productId/comments')
+.get((req,res,next) => {
+    Products.findById(req.params.productId)
+    .then((product) => {
+        if (product != null){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(product.comments);
+        }
+        else{
+            err = new Error('Product ' + req.params.productId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err)); 
+})
+.post((req,res,next) => {
+    Products.findById(req.params.productId)
+    .then((product) => {
+        if (product != null){
+            product.comments.push(req.body);
+            product.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(product);
+            }, (err) => next(err));
+        }
+        else{
+            err = new Error('Product ' + req.params.productId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err)); 
+})
+.put((req,res,next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /products'
+        + req.params.productId + '/comments');
+})
+.delete((req,res,next) => {
+    Products.findById(req.params.productId)
+    .then((product) => {
+        if (product != null){
+            for (var i = (product.comments.length -1); i >= 0; i--){
+                product.comments.id(product.comments[i]._id).remove();
+            }
+            product.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(product);
+            }, (err) => next(err));
+        }
+        else{
+            err = new Error('Product ' + req.params.productId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+productRouter.route('/:productId/comments/:commentId')
+.get((req,res,next) => {
+    Products.findById(req.params.productId)
+    .then((product) => {
+        if (product != null && product.comments.id(req.params.commentId) != null){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(product.comments.id(req.params.commentId));
+        }
+        else if (product == null) {
+            err = new Error('Product ' + req.params.productId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err)
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err)); 
+})
+.post((req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST operation is not allowed on /products/' + req.params.productId
+        + '/comments/' + req.params.commentId);
+})
+.put((req,res,next) => {
+    Products.findById(req.params.productId)
+    .then((product) => {
+        if (product != null && product.comments.Id(req.params.commentId) != null){
+            if (req.body.rating){
+                product.comment.id(req.params.commentId).rating = req.body.rating;
+            }
+            if (req.body.comment){
+                product.comment.id(req.params.commentId).comment = req.body.comment;
+            }
+            product.comments.push(req.body);
+            product.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(product);
+            }, (err) => next(err));
+        }
+        else if (product == null) {
+            err = new Error('Product ' + req.params.productId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err)
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+}) 
+.delete((req,res,next) => {
+    Products.findById(req.params.productId)
+    .then((product) => {
+        if (product != null && product.comments.Id(req.params.commentId) != null){
+            product.comments.id(req.params.commentId).remove();
+            product.save()
+            .then((dish) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(product);
+            }, (err) => next(err));
+        }
+        else if (product == null) {
+            err = new Error('Product ' + req.params.productId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Comment ' + req.params.commentId + ' not found');
+            err.status = 404;
+            return next(err)
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
 module.exports = productRouter;
